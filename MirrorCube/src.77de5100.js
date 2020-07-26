8b752f80c536a32b4cd5f564338cd86c16f0550a
Andrew Nathenson <ColorfulPockets01@gmail.com>
2020-07-26 02:15:37 -0700

    try again

--- MirrorCube/src.77de5100.js
+++ MirrorCube/src.77de5100.js
@@ -42391,7 +42391,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "56120" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "58504" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
